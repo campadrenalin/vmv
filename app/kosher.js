516612d9abf3e0b363d8806660974868512b005b
Philip Horger <philip@inspire.com>
2017-07-06 20:57:50 +0000

    Fully working namespaced approach

--- app/kosher.js
+++ app/kosher.js
@@ -13,17 +13,135 @@ function alphaNum(inputs) {
     return match(inputs, /^[a-zA-Z0-9]*$/);
 }
 
+function promiseWrap(value) {
+    return _.has(value, 'then') ? value
+         : !!value              ? Promise.resolve(value)
+         :                        Promise.reject(value)
+         ;
+}
+
+KosherRelation = Vue.extend({
+    props: ["parent", "inputs", "validator", "args", "mods"],
+    created() {
+        var self = this;
+        for (m in this.mods) {
+            this.mods[m](this);
+        }
+        for (i in this.input_keys) {
+            var input = this.input_keys[i];
+            this.parent.$watch(input, function(newVal, oldVal){
+                Vue.set(self.input_object, input, newVal);
+                self.reset();
+                self.ask();
+            }, { deep: true });
+        }
+    },
+    data: function() {
+        var self = this;
+        return {
+            input_object: {},
+            responses: {},
+            latest_key: undefined,
+            cb: function() {
+                return promiseWrap(self.callback.apply(self, arguments));
+            }
+        }
+    },
+    computed: {
+        dirty  : function() { return this.latest_key != undefined },
+        latest : function() { return this.dirty ? this.responses[this.latest_key] : undefined },
+        pending: function() { return this.dirty && !this.latest },
+
+        payload: function() { return this.latest ? this.latest.payload : undefined },
+        success: function() { return this.latest ? !!this.latest.success : undefined },
+        failure: function() { return this.latest ?  !this.latest.success : undefined },
+
+        input_keys: function() { return this.inputs.split(/ +/) },
+        response_key: function() {
+            var vals = _.values(this.input_object);
+            return vals.length == 1              ? vals[0].toString()
+                :  this.latest_key === undefined ? 0
+                :                                  (this.latest_key + 1) % 10
+                ;
+        },
+        callback: function() {
+            return this.parent.$options.validators[this.validator];
+        },
+
+        description: function() {
+            return this.validator + '("' + this.inputs + '")'
+        },
+        summary: function() {
+            return _.pick(this, 'pending', 'payload', 'success', 'failure', 'dirty');
+        },
+    },
+    methods: {
+        ask: function() {
+            this.reset(true);
+            var resp_key = this.response_key;
+            var promise = this.cb.apply(this, [this.input_object].concat(this.args));
+            promise
+                .then(_.bind(this.resolve, this, resp_key))
+                .catch(_.bind(this.reject, this, resp_key));
+        },
+        reset: function(mark_dirty) {
+            this.latest_key = mark_dirty ? this.response_key : undefined;
+        },
+        store: function(resp_key, ok, data) {
+            this.responses[resp_key] = {
+                payload: data || ok,
+                success: ok,
+            }
+            this.parent.$emit('kosher-response', this.description);
+        },
+        resolve: function(resp_key, data) { this.store(resp_key, true,  data) },
+        reject:  function(resp_key, data) { this.store(resp_key, false, data) },
+    },
+});
+
+KosherField = Vue.extend({
+    data: function() { return {
+        inputs: [],
+    }},
+    computed: {
+        pending: function() { return _.some( this.inputs, _.property('pending')) },
+        success: function() { return _.every(this.inputs, _.property('success')) },
+        failure: function() { return _.some( this.inputs, _.property('failure')) },
+
+        state: function() {
+            return this.pending ? 'pending'
+                :  this.success ? 'valid'
+                :  this.failure ? 'error'
+                :                 ''
+                ;
+        },
+
+        summary: function() {
+            return _.pick(this, 'pending', 'success', 'failure', 'state');
+        },
+    },
+});
+
 Kosher = {
-    data: {
-        '$k_fields': {},
-        '$k_responses': {},
+    data: function() { return {
+        vue_kosher: {
+            'relations': {}, 'r': {},
+            'fields': {},    'f': {},
+        },
+    }},
+    created: function() {
+        this.$on('kosher-response', function(response_name) {
+            Vue.set(this.vue_kosher.r, response_name, this.vue_kosher.relations[response_name].summary);
+            this.vue_kosher.f = _.mapObject(this.vue_kosher.fields, function(val, key){
+                return val.summary
+            });
+        })
     },
     computed: {
         '$k': function() {
-            return {
-                api: this.$k_api,
-                f: _.mapObject(this.$data.$k_fields, _.bind(this.computeField, this)),
-            }
+            var k = this.vue_kosher;
+            k.api = this.$k_api;
+            return k;
         },
         '$k_api': function() {
             return _.extend({},
@@ -33,53 +151,32 @@ Kosher = {
         },
     },
     methods: {
-        mkValidator: function(func, name) {
+        mkValidator: function(validator_callback, validator_name) {
             var self = this;
-            return function(inputs) {
-                var response_key = name+'('+inputs+')';
-                if (!self.$data.$k_responses[response_key])
-                    Vue.set(self.$data.$k_responses, response_key, {});
-                if (!self.$data.$k_fields[inputs])
-                    Vue.set(self.$data.$k_fields, inputs, []);
-
-                self.$data.$k_fields[inputs].push(response_key);
-
-                self.$watch(inputs, function(newVal, oldVal) {
-                    Vue.set(self.$data.$k_responses[response_key], 'latest', newVal);
-                    function store(payload, success) {
-                        Vue.set(self.$data.$k_responses[response_key], newVal, {
-                            payload: payload,
-                            success: success,
-                        });
+            return function() {
+                var relation = new KosherRelation({
+                    propsData: {
+                        parent : self,
+                        inputs : _.find(arguments, _.isString) || '',
+                        args   : _.find(arguments, _.isArray)  || [],
+                        mods   : _.filter(arguments, _.isFunction),
+                        validator : validator_name,
                     }
-
-                    Promise.reject(newVal.length % 2)
-                        .then( function(payload) { store(payload, !!payload) })
-                        .catch(function(payload) { store(payload, false) });
                 });
+                Vue.set(self.vue_kosher.relations, relation.description, relation);
+
+                for (i in relation.input_keys) {
+                    var input = relation.input_keys[i];
+                    var field = self.vue_kosher.fields[input] || new KosherField();
+                    Vue.set(self.vue_kosher.fields, input, field);
+                    field.inputs.push(relation);
+                }
+
+                self.$emit('kosher-response', relation.description);
 
                 return self.$k_api;
             }
         },
-        computeField: function(field, name) {
-            var self = this;
-            var dirty = false
-            var responses = _.map(field, function(r) {
-                var resp_data = self.$data.$k_responses[r];
-                if (resp_data.latest !== undefined) dirty = true;
-                return resp_data[resp_data.latest]
-            });
-            var compact = _.compact(responses);
-            var err = _.find(compact, function(x) { return !x.success });
-
-            return {
-                valid:   dirty && _.every(compact, _.property('success')),
-                error:   dirty && (err ? (err.payload || true) : false),
-                message: dirty && (err && _.isString(err.payload) ? err.payload : undefined),
-                pending: dirty && (compact.length != responses.length),
-                dirty:   dirty,
-            };
-        },
     },
     validators: {
         match: match,
@@ -102,143 +199,5 @@ Kosher = {
         },
     },
 };
-
-VmvAPI = {
-    created() {
-        for (validator_name in this.$options.validators) {
-            this.__proto__[validator_name] = _.bind(this.mkRelation, this, validator_name);
-        }
-        for (modifier_name in this.$options.modifiers) {
-            this.__proto__[modifier_name] = _.bind(this.mkModifier, this, modifier_name);
-        }
-    },
-    methods: {
-        mkRelation: function(validator_name) {
-            var init_args = _.rest(arguments);
-            var rel = new VmvRelation({
-                propsData: {
-                    parent : this,
-                    inputs : _.find(init_args, _.isString) || '',
-                    args   : _.find(init_args, _.isArray)  || [],
-                    mods   : _.filter(init_args, _.isFunction),
-                    callback : this.$options.validators[validator_name]
-                }
-            });
-
-            var rels = this.$options.relations;
-            var inputs = _.keys(rel._inputs);
-            for (i in inputs) {
-                var input = inputs[i];
-                if (rels[input] === undefined)
-                    rels[input] = {};
-                rels[input][validator_name] = rel;
-            }
-            return this;
-        },
-        mkModifier: function(modifier_name) {
-            var args = _.rest(arguments);
-            var mod  = this.$options.modifiers[modifier_name];
-            return function(rel) {
-                mod.apply(rel, args);
-            }
-        }
-    },
-    computed: {
-        '$v': function() {
-            return this.$options.relations;
-        }
-    },
-    validators: {
-        match: match,
-        alphaNum: alphaNum,
-    },
-    modifiers: {
-        throttle: function(ms) { this.ask = _.throttle(this.ask, ms) },
-        debounce: function(ms) { this.ask = _.debounce(this.ask, ms) },
-        delay: function(ms) {
-            var rel   = this;
-            var inner = this.cb;
-            this.cb = function() {
-                var inner_promise = inner.apply(rel, arguments); // Guaranteed to be a promise
-                return new Promise(function(outer_resolve, outer_reject) {
-                    inner_promise
-                        .then(function(data) { _.delay(outer_resolve, ms, data) })
-                        .catch(function(data){ _.delay(outer_reject,  ms, data) })
-                })
-            }
-        },
-    },
-    relations: {}
-};
-
-VmvRelation = Vue.extend({
-    props: ["parent", "inputs", "callback", "args", "mods"],
-    created() {
-        for (m in this.mods) {
-            this.mods[m](this);
-        }
-    },
-    data: function() {
-        var self = this;
-        return {
-            dirty:   false,
-            result:  undefined,
-            error:   undefined,
-            cb: function() {
-                var result = self.callback.apply(self, arguments);
-                return result === true  ? Promise.resolve(result)
-                    :  result === false ? Promise.reject(result)
-                    :                     result;
-            }
-        }
-    },
-    computed: {
-        pending: function() {
-            return this.dirty && !this.result && !this.error;
-        },
-        state: function() {
-            return this.pending ? 'pending'
-                :  this.result  ? 'valid'
-                :  this.error   ? 'error'
-                :                 ''
-                ;
-        },
-        _inputs: function() {
-            var names = this.inputs.split(/ +/);
-            return _.object(
-                names,
-                _.map(names, _.propertyOf(this.parent))
-            );
-        }
-    },
-    watch: {
-        _inputs: function() { this.reset(); this.ask(); }
-    },
-    methods: {
-        ask: function() {
-            this.reset(true);
-            var promise = this.cb.apply(this, _.flatten([this._inputs, this.args]));
-            // TODO: invalidate in-flight work that no longer matches input data
-            promise
-                .then(_.bind(this.resolve, this))
-                .catch(_.bind(this.reject, this));
-        },
-        reset: function(dirty) {
-            this.dirty = dirty || false;
-            this.result = undefined;
-            this.error  = undefined;
-        },
-        resolve: function(data) {
-            this.result = data || true;
-            this.error  = false;
-        },
-        reject: function(data) {
-            this.result = false;
-            this.error  = data || true;
-        },
-    },
-});
-
-VmvAPI.Relation = VmvRelation;
 
 module.exports = Kosher;
